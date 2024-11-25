ff5a4cdb92ebb9878a7f09b88718f097fa5b7bca
Albert Kiama <albertkiamakariuki@gmail.com>
2024-11-25 16:54:49 +0300

    Conect

--- script.js
+++ script.js
@@ -39,3 +39,26 @@ document.addEventListener('DOMContentLoaded', function() {
       } 
     });
   }
+
+  /*====================humbuger================*/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+     hamburger.addEventListener("click", mobileMenu);
+
+     function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+       }
+
+// when we click on hamburger icon its close 
+
+const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+    function closeMenu() {
+       hamburger.classList.remove("active");
+       navMenu.classList.remove("active");
+    }
